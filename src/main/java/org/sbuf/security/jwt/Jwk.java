fca35a754f6cd0002da31a9384ff3e3ce87410f6
hepher <tinni.cris@gmail.com>
2025-04-17 15:05:11 +0200

    Update Jwk.java

--- src/main/java/org/sbuf/security/jwt/Jwk.java
+++ src/main/java/org/sbuf/security/jwt/Jwk.java
@@ -2,20 +2,30 @@ package org.sbuf.security.jwt;
 
 
 import org.sbuf.model.dto.AbstractEntity;
+import com.enelx.bfw.framework.entity.AbstractEntity;
+import com.enelx.bfw.framework.security.jwt.JwtKeys;
+import com.enelx.bfw.framework.util.CertificateUtils;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigInteger;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateEncodingException;
+import java.security.cert.X509Certificate;
 import java.security.interfaces.RSAPublicKey;
 import java.util.Base64;
+import java.util.Collections;
 import java.util.List;
 import java.util.function.Function;
 
 @Getter
 @Setter
 public class Jwk extends AbstractEntity {
+
+    @JsonIgnore
+    private X509Certificate x509Cert;
 
     @JsonProperty(JwtKeys.Parameter.ALGORITHM)
     private String algorithm;
@@ -40,6 +50,9 @@ public class Jwk extends AbstractEntity {
 
     @JsonProperty(JwtKeys.Parameter.X509_CERT_CHAIN)
     private List<String> x509CertList;
+
+    @JsonProperty(JwtKeys.Parameter.X509_CERT_SHA1_THUMBPRINT)
+    private String x509CertTumblingSha1;
 
     @JsonIgnore
     private Function<BigInteger, byte[]> unsignBigIntFunction = bigInt -> {
@@ -73,10 +86,20 @@ public class Jwk extends AbstractEntity {
 
     public Jwk() {}
 
+    public Jwk(X509Certificate x509Cert) throws CertificateEncodingException, NoSuchAlgorithmException {
+        this.x509Cert = x509Cert;
+        algorithm = "RS256";
+        keyType = x509Cert.getPublicKey().getAlgorithm();
+        modulus = Base64.getUrlEncoder().withoutPadding().encodeToString(unsignBigIntFunction.apply(((RSAPublicKey) x509Cert.getPublicKey()).getModulus()));
+        exponent = Base64.getUrlEncoder().withoutPadding().encodeToString(unsignBigIntFunction.apply(((RSAPublicKey) x509Cert.getPublicKey()).getPublicExponent()));
+        keyId = x509Cert.getSerialNumber().toString();
+        x509CertTumblingSha1 = CertificateUtils.getThumbprint(x509Cert);
+        x509CertList = Collections.singletonList(Base64.getEncoder().encodeToString(x509Cert.getEncoded()));
+    }
+
     public Jwk(RSAPublicKey rsaPublicKey) {
         algorithm = "RS256";
         keyType = rsaPublicKey.getAlgorithm();
         modulus = Base64.getUrlEncoder().withoutPadding().encodeToString(unsignBigIntFunction.apply(rsaPublicKey.getModulus()));
         exponent = Base64.getUrlEncoder().withoutPadding().encodeToString(unsignBigIntFunction.apply(rsaPublicKey.getPublicExponent()));
     }
-}
