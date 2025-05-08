b070a91620bdf189faa17e195c8ffa02a6f8e85f
hepher <tinni.cris@gmail.com>
2025-05-08 12:30:31 +0200

    Update ParameterUtils.java

--- src/main/java/org/sbuf/util/ParameterUtils.java
+++ src/main/java/org/sbuf/util/ParameterUtils.java
@@ -6,7 +6,7 @@ public class ParameterUtils {
     public static List<String> getSensitiveDataFields() {
         var sensitiveDataFields = ApplicationContextUtils.evalExpression("${bfw.model.entity.sensitive-data-fields:}");
         if (sensitiveDataFields instanceof String) {
-            return Arrays.asList(((String) sensitiveDataFields).split(","));
+            return Arrays.asList(((String) sensitiveDataFields).split("\\s*,\\s*"));
         }
 
         return null;
